d1f5d374fe8fa6b2fbffb30cdc48ac36340fca51
Mohit Kumar <109014039+Mohitkumar521@users.noreply.github.com>
2023-11-26 13:30:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function shortcut(s1, s2) {
-  // your code here
+    if (!s1 || !s2) {
+        return '';
+    }
+
+    return s1.charAt(0).toUpperCase() + s2.charAt(0).toLowerCase();
 }
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
+
